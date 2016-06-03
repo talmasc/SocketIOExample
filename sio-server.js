89ea5e58d56808e55465bee8f256f526f6ce7eb3
Hillenbrand <eric.hillenbrand@intel.com>
2016-06-03 14:00:40 -0700

    First commit

---
+++ sio-server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var http = require("http");
+var app = express();
+var server = http.createServer(app).listen(3000);
+var io = require("socket.io")(server);
+
+app.use(express.static("./public"));
+
+io.on("connection", function(socket) {
+
+  socket.on("chat", function(message) {
+    socket.broadcast.emit("message", message);
+  });
+
+  socket.emit("message", "Welcome to Cyber Chat");
+});
+
+console.log("Starting a socket app at http://localhost:3000");
